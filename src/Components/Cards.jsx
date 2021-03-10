3a42ac84018fbc3d886b9d927cef32c8ac2ad889
Davin Thomas <rasta.dthomas@outlook.com>
2021-03-10 15:50:58 -0600

    Turned .card subtitles to a_href tags

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -12,38 +12,38 @@ class Cards extends Component {
                     <div className="column">
                         <div className="card">
                             <h3>Frontend Development</h3>
-                            <p>HTML</p>
-                            <p>CSS</p>
-                            <p>Javascript</p>
+                            <a href="HTML">HTML</a>
+                            <a href="CSS">CSS</a>
+                            <a href="Javascript">Javascript</a>
                         </div>
                     </div>
                     <div className="column">
                         <div className="card">
                             <h3>Backend Development</h3>
                             <h3>Servers:</h3>
-                            <p>MongoDB</p>
-                            <p>MySQL</p>
-                            <p>PostgreSQL</p>
+                            <a href="MongoDB">MongoDB</a>
+                            <a href="MySQL">MySQL</a>
+                            <a href="PostgreSQL">PostgreSQL</a>
                         </div>
                     </div>
                     <div className="column">
                         <div className="card">
                             <h3>Programming Languages</h3>
-                            <p>C/C++</p>
-                            <p>Java</p>
-                            <p>Python</p>
-                            <p>Perl</p>
-                            <p>Ruby</p>
-                            <p>C#</p>
+                            <a href="C/C++">C/C++</a>
+                            <a href="Java">Java</a>
+                            <a href="Python">Python</a>
+                            <a href="Perl">Perl</a>
+                            <a href="Ruby">Ruby</a>
+                            <a href="C#">C#</a>
                         </div>
                     </div>
                     <div className="column">
                         <div className="card">
                             <h3>Development Frameworks</h3>
-                            <p>Node.js</p>
-                            <p>Rails</p>
-                            <p>ASP.NET</p>
-                            <p>Bootstrap</p>
+                            <a href="Node.js">Node.js</a>
+                            <a href="Rails">Rails</a>
+                            <a href="ASP.NET">ASP.NET</a>
+                            <a href="Bootstrap">Bootstrap</a>
                         </div>
                     </div>
                 </div>
